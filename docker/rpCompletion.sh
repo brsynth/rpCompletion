256cc5eb3d30e46898d56ad653b8fe67af6071f3
joan <joan.herisson@univ-evry.fr>
2020-05-29 13:28:55 +0200

    rename

---
+++ docker/rpCompletion.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+data_folder=$1
+db_host=$2
+if [[ "$db_host" != "" ]]; then
+  db_host="--link $db_host:$db_host"
+fi
+
+
+docker-compose run --rm -v $data_folder:$data_folder $db_host -w /home/src --entrypoint="" rpcompletion \
+  bash
+
+# docker-compose run --rm rpreader $@
