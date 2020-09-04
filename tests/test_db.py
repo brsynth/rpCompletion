51b6919acfbe2ad5c05a72deb0c9d508174e7aed
joan <joan.herisson@univ-evry.fr>
2020-09-04 15:40:54 +0200

    setup tests

---
+++ tests/test_db.py
@@ -0,0 +1,13 @@
+"""
+Created on Jul 15 2020
+
+@author: Joan Hérisson
+"""
+
+from module import Module
+from rpcompletion import rpCompletion
+
+class Test_File(Module):
+    __test__ = True
+
+    obj       = rpCompletion('localhost')
