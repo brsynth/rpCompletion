c3a93bee98824ed26d072ac7ae8aad953b23c166
joan <joan.herisson@univ-evry.fr>
2020-09-16 20:36:18 +0200

    refactor tests

--- tests/module.py
+++ tests/module.py
@@ -19,13 +19,13 @@ class Module(Main):
 
     mod_name  = 'rpcompletion'
     func_name = 'rp2ToSBML'
-    data_path = 'data'
+    data_path = 'data/lycopene'
     args      = argparse_Namespace()
     # Args have to be set in the order as they be passed
     args.cache              = None
-    args.rp2_pathways       = data_path+'/rp2_pathways.csv'
-    args.rp2paths_compounds = data_path+'/rp2paths_compounds.csv'
-    args.rp2paths_pathways  = data_path+'/rp2paths_pathways.csv'
+    args.rp2_pathways       = data_path+'/1-rp2_pathways.csv'
+    args.rp2paths_compounds = data_path+'/2-rp2paths_compounds.tsv'
+    args.rp2paths_pathways  = data_path+'/3-rp2paths_pathways.csv'
     args.outdir             = tempdir.name
 
     # Useless to sort files since smiles could be equivalent and not equal, then chacksum will be different
@@ -35,42 +35,19 @@ class Module(Main):
             self.assertEqual(os_stat(file).st_size, size)
 
     files = [
-    (tempdir.name+'/'+'rp_10_1_sbml.xml', 37004),
-    (tempdir.name+'/'+'rp_11_1_sbml.xml', 52372),
-    (tempdir.name+'/'+'rp_11_2_sbml.xml', 46612),
-    (tempdir.name+'/'+'rp_12_1_sbml.xml', 47382),
-    (tempdir.name+'/'+'rp_13_1_sbml.xml', 46122),
-    (tempdir.name+'/'+'rp_14_1_sbml.xml', 37243),
-    (tempdir.name+'/'+'rp_15_1_sbml.xml', 35632),
-    (tempdir.name+'/'+'rp_16_1_sbml.xml', 46142),
-    (tempdir.name+'/'+'rp_16_2_sbml.xml', 46247),
-    (tempdir.name+'/'+'rp_17_1_sbml.xml', 44346),
-    (tempdir.name+'/'+'rp_18_1_sbml.xml', 35589),
-    (tempdir.name+'/'+'rp_19_1_sbml.xml', 35485),
-    (tempdir.name+'/'+'rp_1_1_sbml.xml',  41335),
-    (tempdir.name+'/'+'rp_1_2_sbml.xml',  41244),
-    (tempdir.name+'/'+'rp_20_1_sbml.xml', 47233),
-    (tempdir.name+'/'+'rp_21_1_sbml.xml', 47019),
-    (tempdir.name+'/'+'rp_22_1_sbml.xml', 44830),
-    (tempdir.name+'/'+'rp_23_1_sbml.xml', 48252),
-    (tempdir.name+'/'+'rp_23_2_sbml.xml', 56646),
-    (tempdir.name+'/'+'rp_24_1_sbml.xml', 47453),
-    (tempdir.name+'/'+'rp_24_2_sbml.xml', 49259),
-    (tempdir.name+'/'+'rp_25_1_sbml.xml', 56572),
-    (tempdir.name+'/'+'rp_25_2_sbml.xml', 50000),
-    (tempdir.name+'/'+'rp_26_1_sbml.xml', 54496),
-    (tempdir.name+'/'+'rp_27_1_sbml.xml', 64177),
-    (tempdir.name+'/'+'rp_28_1_sbml.xml', 57305),
-    (tempdir.name+'/'+'rp_29_1_sbml.xml', 54528),
-    (tempdir.name+'/'+'rp_2_1_sbml.xml',  27076),
-    (tempdir.name+'/'+'rp_30_1_sbml.xml', 52348),
-    (tempdir.name+'/'+'rp_3_1_sbml.xml',  39517),
-    (tempdir.name+'/'+'rp_4_1_sbml.xml',  49009),
-    (tempdir.name+'/'+'rp_5_1_sbml.xml',  50635),
-    (tempdir.name+'/'+'rp_5_2_sbml.xml',  44875),
-    (tempdir.name+'/'+'rp_6_1_sbml.xml',  45646),
-    (tempdir.name+'/'+'rp_7_1_sbml.xml',  44385),
-    (tempdir.name+'/'+'rp_8_1_sbml.xml',  43248),
-    (tempdir.name+'/'+'rp_8_2_sbml.xml',  37488),
-    (tempdir.name+'/'+'rp_9_1_sbml.xml',  38258)
+    (tempdir.name+'/'+'rp_1_11_sbml.xml',  32358),
+    (tempdir.name+'/'+'rp_1_1_sbml.xml',   32640),
+    (tempdir.name+'/'+'rp_1_6_sbml.xml',   32225),
+    (tempdir.name+'/'+'rp_2_12_sbml.xml',  32355),
+    (tempdir.name+'/'+'rp_2_22_sbml.xml',  32483),
+    (tempdir.name+'/'+'rp_2_2_sbml.xml',   32765),
+    (tempdir.name+'/'+'rp_3_10_sbml.xml',  33763),
+    (tempdir.name+'/'+'rp_3_131_sbml.xml', 34551),
+    (tempdir.name+'/'+'rp_3_132_sbml.xml', 34814),
+    (tempdir.name+'/'+'rp_3_140_sbml.xml', 33353),
+    (tempdir.name+'/'+'rp_3_1_sbml.xml',   34956),
+    (tempdir.name+'/'+'rp_3_261_sbml.xml', 34680),
+    (tempdir.name+'/'+'rp_3_262_sbml.xml', 34942),
+    (tempdir.name+'/'+'rp_3_270_sbml.xml', 33482),
+    (tempdir.name+'/'+'rp_3_2_sbml.xml',   35220),
     ]
