10e99b3954e013268783e7a69683fa49c3f73704
joan <joan.herisson@univ-evry.fr>
2020-10-08 11:23:02 +0200

    refactor(src): rename src folder into rpcompletion
    Needs to change all references to src folder in the project

---
+++ rpcompletion/__init__.py
@@ -0,0 +1,9 @@
+"""
+Created on June 16 2020
+
+@author: Joan Hérisson
+"""
+
+from rpcompletion.rpCompletion import rp2ToSBML, build_args_parser
+
+__all__ = ["rp2ToSBML", "build_args_parser"]
---
+++ rpcompletion/__main__.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from logging      import error as logging_error
+from brs_libs     import rpCache
+from rpcompletion import rp2ToSBML, build_args_parser
+
+
+def _cli():
+    parser = build_args_parser()
+    args  = parser.parse_args()
+
+    args.pubchem_search = args.pubchem_search.lower() in ['true', 't']
+
+    cache = rpCache(db=args.store_mode)
+
+    try:
+        result = rp2ToSBML(cache,
+                           args.rp2_pathways,
+                           args.rp2paths_compounds,
+                           args.rp2paths_pathways,
+                           args.outdir,
+                           int(args.upper_flux_bound),
+                           int(args.lower_flux_bound),
+                           int(args.max_subpaths_filter),
+                           args.pathway_id,
+                           args.compartment_id,
+                           args.species_group_id,
+                           args.sink_species_group_id,
+                           args.pubchem_search)
+        return result
+    except ValueError as e:
+        logging_error(str(e))
+        return 2
+
+
+
+if __name__ == '__main__':
+    _cli()
